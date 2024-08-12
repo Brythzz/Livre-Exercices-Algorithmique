74118046cd0c404c47d5bdb2d9fd20f4ac0e1dc6
Bryth <62302815+Brythzz@users.noreply.github.com>
2024-08-12 16:49:29 +0200

    Finish 2.4

--- 2-jouer-avec-les-mots/src/2-4.ml
+++ 2-jouer-avec-les-mots/src/2-4.ml
@@ -23,25 +23,6 @@ let equal (s1:string) (s2:string) : bool =
     true
   with Exit -> false
 
-let b (n:int) : int =
-  0
-
-let est_facteur_bis (u:string) (v:string) : bool =
-  let n = String.length u in
-  let k = String.length v in
-
-  let i = ref 0 and j = ref 0 in
-
-  while !i <= n && !j <= k do
-    if u.[!i] = v.[!j] then (
-      incr i; incr j
-    ) else (
-      if !j = 0 then incr i
-      else i := 1 + b (!j-1)
-    )
-  done;
-  !j = k-1
-
 let calcul (v:string) : int array =
   let k = String.length v in
   let b = Array.make k 0 in
@@ -50,15 +31,34 @@ let calcul (v:string) : int array =
     let j = ref b.(h-1) in
     let fini = ref false in
     while not !fini do
-      if v.[!j+1] = v.[h-1] then (
+      (* v_{j+1} = v_h *)
+      if v.[!j] = v.[h] then (
         fini := true;
-        b.(h-1) <- !j+1
+        b.(h) <- !j+1
       )
       else if !j = 0 then (
         fini := true;
-        b.(h-1) <- 0
+        b.(h) <- 0
       )
       else j := b.(!j-1)
     done;
   done;
   b
+
+let b (v:string) (n:int) : int =
+  let b = calcul v in b.(n)
+
+let est_facteur_bis (u:string) (v:string) : bool =
+  let n = String.length u in
+  let k = String.length v in
+
+  let i = ref 0 and j = ref 0 in
+  while !i < n && !j < k do
+    if u.[!i] = v.[!j] then (
+      incr i; incr j
+    ) else (
+      if !j = 0 then incr i
+      else i := b v (!j-1)
+    )
+  done;
+  !j = k
