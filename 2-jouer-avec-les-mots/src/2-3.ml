4b3c5b6d1a18a6772a82c4cb2765cb7a293c8d1b
Bryth <62302815+Brythzz@users.noreply.github.com>
2024-08-12 07:25:37 +0200

    Add 2.3

---
+++ 2-jouer-avec-les-mots/src/2-3.ml
@@ -0,0 +1,49 @@
+let longueur_sous_suite a =
+  let n = Array.length a in
+  let l = Array.make n 0 in
+  l.(0) <- 1;
+  for p=1 to n-1 do
+    let max = ref 0 in
+    for j=0 to p-1 do
+      if (a.(j) <= a.(p)) && (l.(j)+1 >= !max)
+        then max := l.(j) + 1
+    done;
+    l.(p) <- !max
+  done;
+  (* Trouver le max *)
+  let longueur = ref 1 in
+  for i=1 to n-1 do
+    if l.(i) > !longueur then
+      longueur := l.(i)
+  done;
+  !longueur
+
+let afficher_sous_suite a =
+  let n = Array.length a in
+  let l = Array.make n 0 in
+  let precedent = Array.make n 0 in
+  l.(0) <- 1;
+  for p=1 to n-1 do
+    let max = ref 0 and prec = ref 0 in
+    for j=0 to p-1 do
+      if (a.(j) <= a.(p)) && (l.(j)+1 >= !max)
+        then (
+          max := l.(j) + 1;
+          prec := j
+        )
+    done;
+    l.(p) <- !max; precedent.(p) <- !prec
+  done;
+  (* Trouver le max *)
+  let longueur = ref 1 and pmax = ref 0 in
+  for i=1 to n-1 do
+    if l.(i) > !longueur then (
+      longueur := l.(i);
+      pmax := i
+    )
+  done;
+  Printf.printf "%d" a.(!pmax);
+  for i=1 to !longueur-1 do
+    pmax := precedent.(!pmax);
+    Printf.printf " %d" a.(!pmax);
+  done;
