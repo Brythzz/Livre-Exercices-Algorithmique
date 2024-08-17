bbe2079bb95f5a81f999737c1dafb3d7095c0aa1
Bryth <62302815+Brythzz@users.noreply.github.com>
2024-08-17 10:56:43 +0200

    Add 3.1

--- 3-strategies-gloutonnes/src/3-1.ml
+++ 3-strategies-gloutonnes/src/3-1.ml
@@ -36,3 +36,49 @@ let repartition (p:int array) (k:int) (c:int) : int array =
   if !report > 0 then failwith "impossible";
   affectation
 
+let repartition2 (p:int array) (k:int) (c:int) : int array =
+  let n = Array.length p in
+  let affectation = Array.make n (-1) in
+
+  let train_libre = ref 0 in
+  for i=0 to k-1 do (* numéro du train *)
+    let nb_voyageurs = ref 0 in
+
+    for j=0 to n-1 do (* numéro du voyageur *)
+      if p.(j) = i then
+        if !train_libre < i then (
+          train_libre := i;
+          nb_voyageurs := 0
+        );
+
+        if !train_libre = k then
+          failwith "impossible";
+
+        affectation.(j) <- !train_libre;
+        nb_voyageurs := !nb_voyageurs + 1;
+
+        if !nb_voyageurs = c then (
+          train_libre := !train_libre + 1;
+          nb_voyageurs := 0
+        )
+      done
+    done;
+  affectation
+
+let repartition_en_ligne (p:int array) (k:int) (c:int) : int array =
+  let n = Array.length p in
+  let affectation = Array.make n (-1) in
+  let t = Array.make k 0 in
+
+  for i=0 to n-1 do (* numéro du voyageur *)
+    let j = ref p.(i) in
+    (* trouver le premier train libre *)
+    while t.(!j) = c && !j < n-1 do incr j done;
+
+    if t.(!j) < c then (
+      affectation.(i) <- !j;
+      t.(!j) <- t.(!j) + 1
+    )
+    else failwith "impossible"
+  done;
+  affectation
