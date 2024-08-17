29a09e318e5711c3cad5d0c1e4b79d4d1780ffcd
Bryth <62302815+Brythzz@users.noreply.github.com>
2024-08-17 20:28:00 +0200

    Add 3.6

---
+++ 3-strategies-gloutonnes/src/3-6.ml
@@ -0,0 +1,10 @@
+let organise (deb:int array) (fin:int array) =
+  let n = Array.length deb in
+
+  let rec aux i j acc =   (* j = dernier ajouté *)
+    if i=n then acc else
+    if deb.(i) >= fin.(j) then
+      aux (i+1) i (i::acc)
+    else
+      aux (i+1) j acc
+  in List.rev (aux 1 0 [0])
